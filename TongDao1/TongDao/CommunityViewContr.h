ff345df3a5e3c859cc317e5647d2814e42cab885
sunyong445 <sunyong@sunyongtekiMacBook-Pro.local>
2013-10-09 09:03:49 +0800

    init

---
+++ TongDao1/TongDao/CommunityViewContr.h
@@ -0,0 +1,24 @@
+//
+//  CommunityViewContr.h
+//  TongDao
+//
+//  Created by sunyong on 13-9-15.
+//  Copyright (c) 2013年 sunyong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommunityViewContr : UIViewController<UIScrollViewDelegate>
+{
+    IBOutlet UIScrollView *contentScrolV;
+    IBOutlet UIImageView *mainImageV;
+    
+    IBOutlet UIButton *leftBt;
+    IBOutlet UIButton *rightBg;
+    NSArray *initAry;
+}
+- (void)loadSubview:(NSArray*)ary;
+- (IBAction)skipPage:(UIButton*)sender;
+
+
+@end
